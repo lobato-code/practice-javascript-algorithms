26a84807edde37e2c944ca10defe3a0bdbeba818
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-05-12 19:50:40 -0500

    [UP] Initial styles

--- pj-browser/script/main.js
+++ pj-browser/script/main.js
@@ -1,3 +1,12 @@
 const countries = [];
+const countryInfoContainer = document.querySelector("#country-info-container");
 
-const key = "UfGZIScs3Ey53umuYGivCWPUO2WilFRoAaInc8Z8";
+fetch("https://restcountries.com/v3.1/all")
+  .then((response) => response.json())
+  .then((data) => {
+    const response = `<p>${data[0].name.common}</p>`;
+    // countryInfoContainer.innerHTML = response;
+  })
+  .catch((error) => console.log(error));
+
+console.log(countryInfoContainer);
