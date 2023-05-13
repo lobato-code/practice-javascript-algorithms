e0efd872874dca518dd86f18294ba656983a8304
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-05-13 11:16:01 -0500

    [UP] Now the searcher is hable to detect words

--- pj-browser/script/main.js
+++ pj-browser/script/main.js
@@ -1,42 +1,49 @@
-const countries = [];
 const countryInfoContainer = document.querySelector("#country-info-container");
 const searchInput = document.querySelector("#search-input");
-function passIndex() {
-  displayCountries(searchInput.value);
-  console.log("Passed");
-}
 
-function displayCountries(index) {
+function displayCountries() {
+  const countries = [];
   fetch("https://restcountries.com/v3.1/all")
     .then((response) => response.json())
     .then((data) => {
-      const response = `
+      function renderCountry(country) {
+        const response = `
     <div class="country-info-flag">
-      <img class="flag-img" src=${data[index].flags.png} alt="" />
-      <p class="flag-name">${data[index].name.common}</p>
+      <img class="flag-img" src=${country.flags.png} alt="" />
+      <p class="flag-name">${country.name.common}</p>
     </div>
     <div class="country-info-details">
-      <p class="datail" ><strong>Capital: </strong> ${data[index].capital}</p>
+      <p class="datail" ><strong>Capital: </strong> ${country.capital}</p>
       <p class="datail" >
-        <strong>Continent: </strong> ${data[index].continents}
+        <strong>Continent: </strong> ${country.continents}
       </p>
       <p class="datail" >
-        <strong>Population: </strong> ${data[index].population}
+        <strong>Population: </strong> ${country.population}
       </p>
       <p class="datail" >
         <strong>Currency: </strong> ${
-          data[index].currencies[Object.keys(data[index].currencies)].name
+          country.currencies[Object.keys(country.currencies)].name
         }
       </p>
       <p class="datail" >
-        <strong>Language: </strong> ${Object.values(data[index].languages)
+        <strong>Language: </strong> ${Object.values(country.languages)
           .toString()
           .split(",")
           .join(", ")}
       </p>
     </div>`;
-      countryInfoContainer.innerHTML = response;
+        countryInfoContainer.innerHTML = response;
+      }
+      countries.push(...data);
       console.log(searchInput.value);
+      function findMatch(wordtToFind, countries) {
+        return countries.filter((country) => {
+          let regex = new RegExp(wordtToFind, "gi");
+          return country.name.common.match(regex);
+        });
+      }
+      const searchResponse = findMatch(searchInput.value, countries);
+      renderCountry(searchResponse[0]);
     })
     .catch((error) => console.log(error));
 }
