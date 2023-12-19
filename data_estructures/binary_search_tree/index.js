4ca84ef51c9b5f88918d93a23de62863d11b502b
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-12-19 07:07:46 -0500

    BST implementation

--- data_estructures/binary_search_tree/index.js
+++ data_estructures/binary_search_tree/index.js
@@ -1,9 +1,3 @@
-// Build a Node class/factory. It should have an attribute for the data it stores as well as its left and right children.
-
-// Build a Tree class/factory which accepts an array when initialized. The Tree class should have a root attribute, which uses the return value of buildTree which you’ll write next.
-
-// Write a buildTree function that takes an array of data (e.g., ) and turns it into a balanced binary tree full of Node objects appropriately placed (don’t forget to sort and remove duplicates!). The buildTree function should return the level-0 root node.
-
 const Tree = require('./Tree');
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
