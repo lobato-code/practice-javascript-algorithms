159a5abcedb57c0a4b3bd327f713ac540842a6fe
zubstack <mt.calderon.alejandro@gmail.com>
2024-06-28 16:39:38 -0500

    [tideman]: fixing addPairs() error

--- challenges/tideman.js
+++ challenges/tideman.js
@@ -4,121 +4,121 @@ let candidates_count = candidates.length;
 let pair_count = 0;
 
 const ranks = [
-    [0, 1, 2],
-    [1, 0, 2],
-    [1, 2, 0],
-    [1, 2, 0],
+  [0, 1, 2], // Voter 1 prefers A > B > C
+  [1, 2, 0], // Voter 2 prefers B > C > A
+  [2, 0, 1], // Voter 3 prefers C > A > B
 ];
+
 const voters = ranks.length;
 
 const pairs = Array.from(
-    { length: (candidates_count * (candidates_count - 1)) / 2 },
-    () => {
-        return { winner: null, loser: null };
-    }
+  { length: (candidates_count * (candidates_count - 1)) / 2 },
+  () => {
+    return { winner: null, loser: null };
+  }
 );
 
 const preferences = Array.from({ length: candidates_count }, () =>
-    Array(candidates_count).fill(0)
+  Array(candidates_count).fill(0)
 );
 const locked = Array.from({ length: candidates_count }, () =>
-    Array(candidates_count).fill(false)
+  Array(candidates_count).fill(false)
 );
 
 function recordPreferences() {
-    for (let i = 0; i < voters; i++) {
-        for (let j = 0; j < candidates_count; j++) {
-            for (let k = j + 1; k < candidates_count; k++) {
-                // console.log(`${ranks[i][j]} -> ${ranks[i][k]}`);
-                preferences[ranks[i][j]][ranks[i][k]] += 1;
-            }
-        }
+  for (let i = 0; i < voters; i++) {
+    for (let j = 0; j < candidates_count; j++) {
+      for (let k = j + 1; k < candidates_count; k++) {
+        // console.log(`${ranks[i][j]} -> ${ranks[i][k]}`);
+        preferences[ranks[i][j]][ranks[i][k]] += 1;
+      }
     }
-    printPreferences()
+  }
+  printPreferences();
 }
 
 function addPairs() {
-    for (let i = 0; i < candidates_count; i++) {
-        for (let j = i + 1; j < candidates_count; j++) {
-            if (i != j && preferences[i][j] != preferences[j][i]) {
-                if (preferences[i][j] > preferences[j][i]) {
-                    pairs[i].winner = i;
-                    pairs[i].loser = j;
-                } else {
-                    pairs[i].winner = j;
-                    pairs[i].loser = i;
-                }
-                pair_count = pair_count + 1      
-            }
+  let index = 0;
+  for (let i = 0; i < candidates_count; i++) {
+    for (let j = i + 1; j < candidates_count; j++) {
+      if (preferences[i][j] != preferences[j][i]) {
+        if (preferences[i][j] > preferences[j][i]) {
+          pairs[index].winner = i;
+          pairs[index].loser = j;
+        } else {
+          pairs[index].winner = j;
+          pairs[index].loser = i;
         }
+        pair_count += 1;
+        index += 1;
+      }
     }
-    printPairs()
+  }
+  printPairs();
 }
 
 function printPairs() {
-    for (let i = 0; i < pair_count; i++) {
-        console.log(
-            `Pair: ${candidates[pairs[i].winner]} ->  ${candidates[pairs[i].loser]}`
-        );
-    }
-    // console.log('pairs', pairs);
-
+  for (let i = 0; i < pair_count; i++) {
+    console.log(
+      `Pair: ${candidates[pairs[i].winner]} ->  ${candidates[pairs[i].loser]}`
+    );
+  }
+  // console.log('pairs', pairs);
 }
 
 function printPreferences() {
-    for (let i = 0; i < candidates_count; i++) {
-        for (let j = 0; j < candidates_count; j++) {
-            if (i != j) {
-                console.log(
-                    `Preference: ${candidates[i]} -> ${candidates[j]}: ${preferences[i][j]}`
-                );
-            }
-        }
+  for (let i = 0; i < candidates_count; i++) {
+    for (let j = 0; j < candidates_count; j++) {
+      if (i != j) {
+        console.log(
+          `Preference: ${candidates[i]} -> ${candidates[j]}: ${preferences[i][j]}`
+        );
+      }
     }
-    // console.log('preferences', preferences);
+  }
+  // console.log('preferences', preferences);
 }
 
 function sortPairs() {
-
-    for (let i = 0; i < pair_count - 1; i++) {
-        for (let j = 0; j < pair_count - 1; j++) {
-            if (
-                preferences[pairs[j].winner][pairs[j].loser] <
-                preferences[pairs[j + 1].winner][pairs[j + 1].loser]
-            ) {
-                let temp = pairs[j + 1];
-                pairs[j + 1] = pairs[j];
-                pairs[j] = temp;
-            }
-        }
+  for (let i = 0; i < pair_count - 1; i++) {
+    for (let j = 0; j < pair_count - 1; j++) {
+      if (
+        preferences[pairs[j].winner][pairs[j].loser] <
+        preferences[pairs[j + 1].winner][pairs[j + 1].loser]
+      ) {
+        let temp = pairs[j + 1];
+        pairs[j + 1] = pairs[j];
+        pairs[j] = temp;
+      }
     }
+  }
 
-    printPairs()
-    return 0;
+  printPairs();
+  return 0;
 }
 
 function lockPairs() {
-    // console.log('pairs', pairs);
-    for (let i = 0; i < pair_count; i++) {
-      locked[pairs[i].winner][pairs[i].loser] = true;
-    }
-    console.log('locked', locked);
+  // console.log('pairs', pairs);
+  for (let i = 0; i < pair_count; i++) {
+    locked[pairs[i].winner][pairs[i].loser] = true;
+  }
+  console.log('locked', locked);
 }
 
 function printWinner() {
-    for (let i = 0; i < candidates_count; i++) {
-        let clean = true;
-        for (let j = 0; j < candidates_count; j++) {
-            if (locked[j][i] == true) {
-                console.log(`${candidates[j]}->${candidates[i]}`);
-                clean = false;
-            }
-        }
-        if (clean) {
-            console.log('Winner', candidates[i]);
-            return;
-        }
+  for (let i = 0; i < candidates_count; i++) {
+    let clean = true;
+    for (let j = 0; j < candidates_count; j++) {
+      if (locked[j][i] == true) {
+        console.log(`${candidates[j]}->${candidates[i]}`);
+        clean = false;
+      }
     }
+    if (clean) {
+      console.log('Winner', candidates[i]);
+      return;
+    }
+  }
 }
 
 recordPreferences();
